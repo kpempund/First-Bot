acf5c5ed446dc8ac3a3b3c1a7236de6fc053bdda
kpempund <kpempund@gmail.com>
2021-11-09 01:51:11 +0700

    Reset

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-const config = require("./Data/config.json");
-
-const intents = new Discord.Intents(32767);
-
-const client = new Discord.Client({ intents });
-
-client.login("config.token");
