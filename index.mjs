67b0b19f21a69938b50a0c83d62eca0f470d6120
kpempund <kpempund@gmail.com>
2021-11-09 02:36:34 +0700

    n1

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
-import DiscordJS from `discord.js`
+import DiscordJS from "discord.js"
 import { Intents } from "discord.js"
-import dotenv from `dotenv`
+import dotenv from "dotenv"
 dotenv.config()
 
 const client = new DiscordJS.Client({
@@ -15,7 +15,7 @@ client.on(`ready`, () => {
 })
 
 client.on(`messageCreate`, (message) => {
-    if(message.content === `ping`) {
+    if(message.content === `hello`) {
         message.reply({
             content: `hi`,
 
